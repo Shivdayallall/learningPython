0c1b70b78ff432378445a2d18f61934d77ee124d
shiv lall <shivlall0009@gmail.com>
2024-05-06 08:40:22 -0400

    completed day 1 of the 100 days of python

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,14 @@
+#1- create a greeting for the program
+print("Good Day User! Welcome to the band name generator!")
+
+#2- ask the user for the city they grew up in
+city_name = input("what is the name of the city you grew up in?\n ")
+
+#3- ask the user for the name of a pet
+pet_name = input("what is your pet's name?\n ")
+
+#4- combine the name of the city and the name of there pet to generate the band name
+band_name = city_name + " " + pet_name
+print("your new band name will be " + band_name)
+
+#5- add cursor to show new line
---
+++ Day1/main.py
@@ -0,0 +1,27 @@
+# print to console
+print("Hello, world")
+print("1. Mix 500g of flour 10g yest, and 300ml water in a bowl")
+print("2. knead the dough for 10 minutes")
+print("3. Add 3g of salt")
+print("4. Leave to rise for 2 hours")
+print("5. Bake for 200 degree c for 30 minutes")
+
+#line break
+print("hello world \nhello again")
+
+# string concatation
+print("hello" + " shiv")
+print("hello" + " " + "bai")
+
+
+# get input
+#input("what is your name?")
+
+print(len("shivdayal"))
+print("hello" " " + input("what is your name my friend?"))
+
+
+
+#variables are created w/o using a key word. python uses snake_cake to name variables
+name = input("what is your name boi? ")
+print(name)
