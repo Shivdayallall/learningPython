dae7b608473a57babaf7aa146973ad01abf7f377
Shivdayal Lall <shivlall0009@gmail.com>
2024-05-13 19:54:20 -0400

    completed day 6

---
+++ Day6/main.py
@@ -0,0 +1,14 @@
+# functions are used to encapsulate code and run code block repeatedly.
+# function in python are defined using the def key word.
+
+def greet():
+    print("hello, shiv")
+
+
+greet()
+
+# while loop are used to iterate over a task until the condition becomes false or true.
+counter = 0
+while counter < 6:
+    print("Hello people")
+    counter += 1
