c6ee3d9840ee7f08f83957ee48527e6976a344cd
shiv lall <shivlall0009@gmail.com>
2024-05-14 17:38:14 -0400

    fixed the index out of range error for words staring with the letter 2

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,13 +1,19 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+#BUG- index out of range error is thrown when a word that starts with the letter z is being encoded, to fix the bug copy and paste the value in the alphabet again
 
 #direction = input("Type 'encode' to encrypt, OR  type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
 #TODO-1: Create a function called 'encrypt' that takes the text and shift as inputs.
 
 
 def encrypt(plain_text, shift_amount):
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+
     cipher_text = ""
     for letter in plain_text:
         get_position_of_each_letter = alphabet.index(letter)
@@ -17,11 +23,7 @@ def encrypt(plain_text, shift_amount):
     print(f"Encoded text is: {cipher_text}")
 
 
-encrypt(text, shift)
-
-
-
-
-#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+
+encrypt(text, shift)
