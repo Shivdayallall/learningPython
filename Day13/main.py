08bcc95cc988469002f1cb3acb7edf0163d28421
shiv lall <shivlall0009@gmail.com>
2024-05-17 20:11:06 -0400

    completed day 13

---
+++ Day13/main.py
@@ -0,0 +1,8 @@
+#Day 13 was just a day to talk about debugging.
+#Step 1 - Understand what the problem is asking. You can't solve the proble without first understanding what is happening.
+#Step 2 - Know your input and out. Know what the problem needs for the input and what is the expected output.
+#Step 3 - Understand what each line of code is doing, not what you think it is doing.
+#Step 4 - Run the code often, in order to catch bugs as they appear.
+#Step 5 - Use print.
+#Step 6 - Use break points.
+#Step 7 - If all else fail bring down the hammer by using a DeBugger.
