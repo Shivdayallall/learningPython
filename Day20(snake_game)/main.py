799dc2bd955a20a86b823d33f9a56e1f8fa12b4a
shiv lall <shivlall0009@gmail.com>
2024-07-17 19:27:26 -0400

    complete first day of snake game

--- Day20(snake_game)/main.py
+++ Day20(snake_game)/main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,29 +8,13 @@ screen.bgcolor("black")
 screen.title("My snake game")
 screen.tracer(0)
 
-# segment_1 = Turtle("square")
-# segment_1.color("white")
-#
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
-#
-# segment_3 = Turtle("square")
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
+snake = Snake()
 
-# PROGRAMATIC WAY TO DO ABOVE TASK IS TO USE A LOOP
-
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
@@ -37,11 +22,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_number in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_number - 1].xcor()
-        new_y = segments[seg_number - 1].ycor()
-        segments[seg_number].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
+
+
+
 
 
 
